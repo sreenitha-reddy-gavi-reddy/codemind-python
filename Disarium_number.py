7c92713ecd8d99336de43b81042332d8fd4ae788
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-05-22 17:29:00 +0530

    n=int(input()) length=len(str(n)) temp=n s=0 while temp>0:     i=temp%10     s=s+int(i**length)     temp=temp//10     length=length-1 if s==n:     print("True") else:     print("False")

---
+++ Disarium_number.py
@@ -0,0 +1,13 @@
+n=int(input())
+length=len(str(n))
+temp=n
+s=0
+while temp>0:
+    i=temp%10
+    s=s+int(i**length)
+    temp=temp//10
+    length=length-1
+if s==n:
+    print("True")
+else:
+    print("False")
