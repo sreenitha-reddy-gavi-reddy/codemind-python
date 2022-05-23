3b5b551a21e81dac774d94830a417d4e6513db5c
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-05-23 21:58:40 +0530

    n=int(input()) for i in range(n,0,-1):     for j in range(1,i+1):         print(chr(i+64),end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,5 @@
 n=int(input())
-for i in range(n):
-    for j in range(n):
-        print(chr(i+65),end=' ')
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(chr(i+64),end=' ')
     print()
