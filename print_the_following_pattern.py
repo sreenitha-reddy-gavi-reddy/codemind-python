73ff90fdd1a67a1c2a904a230ebe74765e5e7862
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-29 21:07:55 +0530

    n=int(input()) m=0 for i in range(1,n+1,1):     for j in range(1,n-1,1):         print(j,end="")         if j==n-2:             m=j             for k in range(m-1,0,-1):                 print(k,end="")
        print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -5,6 +5,7 @@ for i in range(1,n+1,1):
         print(j,end="")
         if j==n-2:
             m=j
-            for k in range(1,m,1):
+            for k in range(m-1,0,-1):
                 print(k,end="")
+                
     print()
