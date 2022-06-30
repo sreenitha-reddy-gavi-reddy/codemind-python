dc2f0c7a10546e76c04fd618d31dc49198675aa6
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-30 22:09:34 +0530

    row=int(input()) for i in range(1,row+1):     for j in range(1,row+1-i):         print(' ',end='')     for j in range(1,i+1):         print(j,end='')     for j in range(i-1,0,-1):         print(j,end='')     print()

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,9 @@
-n=int(input())
-for i in range(1,n+1,1):
-    for j in range(1,n+1,1):
-        print(i,end=' ')
+row=int(input())
+for i in range(1,row+1):
+    for j in range(1,row+1-i):
+        print(' ',end='')
+    for j in range(1,i+1):
+        print(j,end='')
+    for j in range(i-1,0,-1):
+        print(j,end='')
     print()
